cde6a510e679c71d31cc802687f3f8ed6b23efd2
YoMiKru <yohan.minodier@gmail.com>
2026-09-24 11:29:07 +0200

    fixes login_action.php

--- VISA_2/codes/web/code/public/pages/login_action.php
+++ VISA_2/codes/web/code/public/pages/login_action.php
@@ -5,24 +5,27 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    
-    //$db = new SQLite3('/var/www/data/comptes.sqlite');
-    //$stmt = $db->prepare("SELECT * FROM utilisateurs WHERE EMAIL = :email AND PASS = :pass");
-    //$stmt->bindValue(':email', $email, SQLITE3_TEXT);
-    //$stmt->bindValue(':pass', $password, SQLITE3_TEXT);
-    //$result = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+    // Configuration de MariaDB (a séparer pour la suite dans src ?)
+    $db = "db";
+    $dbhost = "mariadb";
+    $dbport = 3306;
+    $dbuser = "mariadb_user";
+    $dbpassword = "05102006";
 
-    $db = "db"
-    $dbhost = "mariadb"
-    $dbport = 3306
-    $dbuser = "mariadb_user"
-    $dbpassword = "05102006"
+    $pdo = new PDO(
+        "mysql:host=$dbhost;port=$dbport;dbname=$db;charset=utf8mb4",
+        $dbuser,
+        $dbpassword
+    );
 
-    $pdo = new PDO('mysql:host'.$dbhost';port='.$dbport';dbname='.$db'', $dbuser, $dbpassword)
-    $pdo->prepare("SELECT * FROM utilisateurs WHERE EMAIL = :email AND PASS = :pass")
-    $pdo->bindValue(':email', $email);
-    $pdo->bindValue(':pass', $password);
-    $result = $pdo->execute();
+
+    $stmt = $pdo->prepare("SELECT * FROM Utilisateurs WHERE Email = :email AND Pass = :pass");
+    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':pass', $password);
+
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
     $status = "Échec";
     $role = "N/A";
@@ -30,17 +33,27 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     if ($result) {
         $_SESSION['email'] = $email;
         $_SESSION['statut'] = $result['STATUT'];
+
         $status = "Succès";
         $role = $result['STATUT'];
+        
         $redirect = "../index.php";
     } else {
         $_SESSION['login_error'] = "Email ou mot de passe incorrect.";
+        
         $redirect = "../connexion.php";
     }
 
     //Écriture du log
     $log = fopen("/var/www/logs/connexion.log", "a");
-    fwrite($log, date("Y-m-d H:i:s") . " - $email - " . $_SERVER['REMOTE_ADDR'] . " - $status - Rôle : $role\n");
+
+    fwrite($log, 
+            date("Y-m-d H:i:s") . 
+            " - $email - " . 
+            $_SERVER['REMOTE_ADDR'] . 
+            " - $status - Rôle : $role\n"
+            );
+            
     fclose($log);
 
     //Redirection après log
